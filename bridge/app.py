4ba21148cf8c80f1ab7b78cf3f0ed4962bd6a339
Isaac Avram <avrisaac555@gmail.com>
2017-09-08 14:44:20 -0700

    Added event loop to app.py

--- bridge/app.py
+++ bridge/app.py
@@ -1,9 +1,24 @@
-from . import torrent, tracker
+from . import torrent, tracker, peer
 import asyncio
+import logging
 import random
 
-peer_id = tracker.generate_peer_id(debug=True)
+DEBUG = True
+
+peer_id = tracker.generate_peer_id(debug=DEBUG)
 listen_port = random.randrange(6881, 6889)
 
+
+async def start_app(loop: asyncio.AbstractEventLoop):
+    pass
+
+
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+
+    if DEBUG:
+        loop.set_debug(DEBUG)
+        logging.getLogger('asyncio').setLevel(logging.DEBUG)
     
+    loop.create_task(start_app())
+    loop.run_forever()
