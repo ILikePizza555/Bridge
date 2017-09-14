e62390bbf8a3cf70b72632ba0913e7e6779306f7
Isaac Avram <avrisaac555@gmail.com>
2017-09-13 19:29:04 -0700

    Fixed the tracker unit test

--- src/tests/test_tracker.py
+++ src/tests/test_tracker.py
@@ -1,19 +1,14 @@
-from bridge.data import TorrentData
+from bridge.data import Torrent
 from bridge.tracker import announce_tracker
 from bridge.peer import generate_peer_id
-from collections import namedtuple
 import pytest
-
-
-_TestTorrent = namedtuple("Torrent", ["data"])
 
 
 @pytest.mark.asyncio
 async def test_announce():
-    t = _TestTorrent(data=TorrentData("test.torrent"))
+    t = Torrent("test.torrent", generate_peer_id(True), 8301)
 
-    tracker_response = await announce_tracker(t, t.data["announce"].decode("utf-8"),
-                                              generate_peer_id(True), 8301)
+    tracker_response = await announce_tracker(t, t.data.announce[0][0])
     
     print(tracker_response)
     assert tracker_response.sucessful is True
